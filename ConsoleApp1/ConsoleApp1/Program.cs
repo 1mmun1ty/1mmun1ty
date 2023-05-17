82c34c7c2852229ca835125a077936971a564c9c
1mmun1ty <100227332+1mmun1ty@users.noreply.github.com>
2023-05-17 11:36:21 +0300

    цикл while и random

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -30,15 +30,36 @@ string otchestvo = Console.ReadLine();
 Console.WriteLine(name + " " + surname + " " + otchestvo);
 
 
-
+Random rnd = new Random();
 float hpplayer = 100;
 float hpenemy = 100;
-float dmgplayer = 7;
-float dmgenemy = 4;
+float dmgplayer = rnd.Next(10, 15);
+float dmgenemy = rnd.Next(1, 15);
 float defplayer = 2;
 float defenemy = 4;
 float dmggivehero = dmgenemy/defplayer;
 float dmggiveenemy = dmgplayer/defenemy;
+while (hpplayer > 0 && hpenemy > 0)  // цикл будет работать, пока оба персонажа не погибнут
+{
+    hpplayer -= dmggiveenemy;
+    hpenemy -= dmggivehero;
+    dmgplayer = rnd.Next(10, 15);
+    dmgenemy = rnd.Next(1, 15);
+    dmggivehero = dmgenemy / defplayer;
+    dmggiveenemy = dmgplayer / defenemy;
+    Console.WriteLine("У игрока осталось: " + hpplayer);
+    Console.WriteLine("У врага осталось: " + hpenemy);
+}
+
+// после того, как один из персонажей погибнет, цикл завершится
+/*if (hpplayer <= 0)
+{
+    Console.WriteLine("Враг победил!");
+}
+else
+{
+    Console.WriteLine("Вы победили врага!");
+}*/
 float tphp = hpplayer - dmggivehero;
 float tpenemy = hpenemy - dmggiveenemy;
 Console.WriteLine("У игрока осталось: " + tphp);
